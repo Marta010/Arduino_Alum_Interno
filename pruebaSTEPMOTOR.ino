9a465151784f51e37d7021018df5889bf74b1e1e
Marta010 <diazvilamarta@gmail.com>
2017-03-07 18:34:51 +0100

    Add files via upload
    Arduino: step motor en ambos sentidos

---
+++ pruebaSTEPMOTOR.ino
@@ -0,0 +1,19 @@
+
+#include <Stepper.h>
+Stepper horizontal (50,8,9,10,11); // horizontal step motor
+//horizontal.setSpeed(30);
+
+void setup() {
+
+ horizontal.setSpeed(200);
+   Serial.begin(9600);
+}
+
+void loop() {
+  
+  horizontal.step(300);
+  //delay(1000);
+  horizontal.step(-300);
+ // delay(1000);
+
+}
